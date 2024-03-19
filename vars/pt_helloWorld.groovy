1198237d8ca1385943356b397ba080c476c9e79e
cccaternberg <acaternberg@cloudbees.com>
2024-03-19 08:59:30 +0100

    add agent

--- vars/pt_helloWorld.groovy
+++ vars/pt_helloWorld.groovy
@@ -1,5 +1,5 @@
 // vars/helloWorld.groovy
-def call(firstName,lastName) {
+def call(String firstName,String lastName) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     echo "Hello ${firstName} ${lastName}"
     echo "Have a great day!"
