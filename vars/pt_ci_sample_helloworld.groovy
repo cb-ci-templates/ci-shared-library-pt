09312555b993e67bb6894fba749c8f050e77b79e
cccaternberg <acaternberg@cloudbees.com>
2024-03-19 09:05:05 +0100

    add agent

--- vars/pt_ci_sample_helloworld.groovy
+++ vars/pt_ci_sample_helloworld.groovy
@@ -1,24 +1,29 @@
 def call(Map pipelineParams) {
     pipeline {
-        agent {
-            kubernetes {
-                yaml libraryResource("podtemplates/${pipelineParams.k8_agent_yaml}")
-            }
-        }
+        agent none
         stages {
             stage('Say Hello') {
+                agent {
+                    kubernetes {
+                        yaml libraryResource("podtemplates/${pipelineParams.k8_agent_yaml}")
+                    }
+                }
                 steps {
                     pt_helloWorld(pipelineParams.firstName,pipelineParams.lastName)
                     sleep 10
-                    checkpoint 'Hello'
                 }
             }
             stage('Say By') {
+                agent {
+                    kubernetes {
+                        yaml libraryResource("podtemplates/${pipelineParams.k8_agent_yaml}")
+                    }
+                }
                 steps {
                     echo "By: ${pipelineParams.firstName}, ${pipelineParams.lastName}"
-                    checkpoint 'By'
-                }
+                 }
             }
+            checkpoint 'By'
         }
     }
 }
